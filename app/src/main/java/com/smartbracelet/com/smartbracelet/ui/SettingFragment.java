58d963cccde60e6636234b01fb7fb70f22618cc3
lyang850108 <lyang850108@163.com>
2016-04-12 21:30:38 +0800

    Fragment_bt added
    Fragment_bt added

--- app/src/main/java/com/smartbracelet/com/smartbracelet/ui/SettingFragment.java
+++ app/src/main/java/com/smartbracelet/com/smartbracelet/ui/SettingFragment.java
@@ -66,7 +66,7 @@ public class SettingFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        return inflater.inflate(R.layout.fragment_bluetooth, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
