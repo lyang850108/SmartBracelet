9ce11d0e49d16f3a346d9dc04c720bbf928a86cf
lyang850108 <lyang850108@163.com>
2016-05-25 22:53:26 +0800

    Add the main_menu activity
    Add the main_menu activity

---
+++ app/src/main/java/com/smartbracelet/com/smartbracelet/activity/MainMenuActivity.java
@@ -0,0 +1,15 @@
+package com.smartbracelet.com.smartbracelet.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.smartbracelet.com.smartbracelet.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
